1a6a4255151b565e49c5939b4ba2f74ba97762a4
magdielian-code <oziomaagaecheta81@gmail.com>
2025-01-26 15:34:11 +0100

    completed the backedn storage tasks

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,20 +2,19 @@
 
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN p_user_id INT)
 BEGIN
     DECLARE avg_score DECIMAL(10,2);
     
     -- Compute average score
     SELECT AVG(score) INTO avg_score
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE user_id = p_user_id;
     
     -- Update average score for the user
     UPDATE users
     SET average_score = avg_score
-    WHERE id = user_id;
-END;
-//
+    WHERE id = p_user_id;
+END //
 
 DELIMITER ;
